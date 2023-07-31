481623cb441d65b2914af6304f96bf9b371f8393
BoczeJr <bocsendiaye@gmail.com>
2023-07-31 15:40:53 +0000

    ajout de l'index html

---
+++ index.php
@@ -0,0 +1,186 @@
+<!DOCTYPE html>
+<html>
+<head>
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Sigmar&display=swap" rel="stylesheet">
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<title>Accueil</title>
+</head>
+<body>
+
+	<header>
+		
+		<div id="entete">
+			<div id="logo">
+				<img src="images/Dieyna_Couture_Off_1.png">
+			</div>
+
+			<div id="menu">
+				<ul>
+					<li><a href="index.php">Accueil</a></li>
+					<li>
+						<a href="#modeles">Nos Modèles</a>
+					<ul>
+						<li><a href="#modeles_femmes">POUR FEMMES</a></li>
+						<li><a href="#modeles_hommes">POUR HOMMES</a></li>
+
+					</ul>
+					</li>
+					<li><a href="#footer">A propos</a></li>
+
+				</ul>
+			</div>
+
+		</div>
+
+	</header>
+
+	<section id="section_1">
+		<div id="texte">
+			Bienvenue chez <h1 id="nom">DIEYNA COUTURE <span id="suite" >MOU MAME DABAKH </span></h1>
+			<div id="dessins">
+				<img id="machine" src="images/15.png" alt="#">
+			</div>
+			<p id="slogan"><i>Notre Super Pouvoir Est De Coudre </i></p>
+
+		</div>
+
+		<div id="images_3">
+
+			<img class="images_123" src="images/13_bis.png" alt="#">
+			<img class="images_123" src="images/14_bis.png" alt="#">
+			<img class="images_123" src="images/8_bis.png" alt="#">
+
+
+		</div>
+
+	</section>
+
+	<section id="modeles">
+		<h1>NOS MODELES POUR FEMMES</h1>
+
+
+		<div id="modeles_femmes">
+		
+		
+			<div><img id="image_logo" src="images/Dieyna_Couture_Off_1.png"><img id="images_femmes" src="images/modele_2.jpg" alt="#"></div>
+			<div><img id="image_logo" src="images/Dieyna_Couture_Off_1.png"><img id="images_femmes" src="images/modele_13.jpg" alt="#"></div>
+			<div><img id="image_logo" src="images/Dieyna_Couture_Off_1.png"><img id="images_femmes" src="images/modele_6.jpg" alt="#"></div>
+			<div><img id="image_logo" src="images/Dieyna_Couture_Off_1.png"><img id="images_femmes" src="images/modele_8.jpg" alt="#"></div>
+			<div><img id="image_logo" src="images/Dieyna_Couture_Off_1.png"><img id="images_femmes" src="images/modele_9.jpg" alt="#"></div>
+			<div><img id="image_logo" src="images/Dieyna_Couture_Off_1.png"><img id="images_femmes" src="images/modele_10.jpg" alt="#"></div>
+			<div><img id="image_logo" src="images/Dieyna_Couture_Off_1.png"><img id="images_femmes" src="images/modele_11.png" alt="#"></div>
+			<div><img id="image_logo" src="images/Dieyna_Couture_Off_1.png"><img id="images_femmes" src="images/modele_12.jpg" alt="#"></div>
+
+		</div>
+
+	</section>
+
+	<section id="modeles">
+
+		<h1>NOS MODELES POUR HOMMES</h1>
+		<div id="modeles_hommes">
+			<div><img id="image_logo" src="images/Dieyna_Couture_Off_1.png"><img id="images_hommes" src="images/homme_7.jpg" alt="#"></div>
+			<div><img id="image_logo" src="images/Dieyna_Couture_Off_1.png"><img id="images_hommes" src="images/homme_5.jpg" alt="#"></div>
+			<div><img id="image_logo" src="images/Dieyna_Couture_Off_1.png"><img id="images_hommes" src="images/homme_3.jpg" alt="#"></div>
+			<div><img id="image_logo" src="images/Dieyna_Couture_Off_1.png"><img id="images_hommes" src="images/homme_4.jpg" alt="#"></div>
+			<div><img id="image_logo" src="images/Dieyna_Couture_Off_1.png"><img id="images_hommes" src="images/homme_12.jpg" alt="#"></div>
+			<div><img id="image_logo" src="images/Dieyna_Couture_Off_1.png"><img id="images_hommes" src="images/homme_15.jpg" alt="#"></div>
+			<div><img id="image_logo" src="images/Dieyna_Couture_Off_1.png"><img id="images_hommes" src="images/homme_13.jpg" alt="#"></div>
+			<div><img id="image_logo" src="images/Dieyna_Couture_Off_1.png"><img id="images_hommes" src="images/homme_10.jpg" alt="#"></div>
+		</div>
+
+
+	</section>
+
+
+
+
+
+</body>
+
+<footer id="footer">
+
+	<div class="footer_inside">
+		<div class="footer_div">
+
+			<img id="Logo_footer" src="images/Dieyna_Couture_Off_1.png" alt="#">
+
+		</div>
+		<hr>
+		<div class="footer_div">
+			<p class="titres_footer"><b>Nos Services</b></p>
+			<ul>
+				<li><a href="#">Couture</a></li>
+				<li><a href="#">Laits de corps</a></li>
+				<li><a href="#">Maquillages</a></li>
+				<li><a href="#">Shampooing</a></li>
+				<li><a href="#">Parfums</a></li>
+			</ul>
+	
+		</div>
+		<hr>
+		<div class="social">
+
+			<p class="titres_footer"><b>Contactez-nous ici</b></p>
+			<ul>
+				<li><a href=""><img id="reseau_social" src="images/WhatsApp.png" alt="#"> WhatsApp</a></li>
+				<li><a href=""><img id="reseau_social" src="images/Instagram.png" alt="#">Instagram</a></li>
+				<li><a href=""><img id="reseau_social" src="images/Tiktok.png" alt="#">Tik Tok</a></li>
+				<p id="coordonees"> <span id="numeros">Nos Numéros :</span>  +221 77 657 96 25 | +221 70 874 67 97</p>
+				<h1> Scat Urbam, Près de Hôpital Américain </h1>
+				
+			</ul>
+	
+		</div>
+
+		<hr>
+		<div class="whitespace">
+			<div class="mentions"><a id="legal" href="#">Mentions Légales</a> | &copy; 2023<span class="bold"></span></div>
+		</div>
+
+
+
+
+
+
+
+	</div>
+
+
+
+
+
+</footer>
+
+
+
+
+
+</html>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
